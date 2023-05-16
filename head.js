7e9e716f76764e28c7119d603a751760f6032c5e
karvok <kayvee123@gmail.com>
2023-05-16 09:50:22 -0600

    Add head.js

---
+++ head.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(array) {
+  return array.shift();
+};
+
+// Test Code
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["One"]), "One");
+assertEqual(head([]), undefined);
