942a92f00e7767501a982e299a5e8b87743bb20e
karvok <kayvee123@gmail.com>
2023-05-22 13:41:20 -0600

    Add countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,44 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  const result = {};
+  for (let letter of sentence) {
+    if (letter !== " ") {
+      if (result[letter]) {
+        result[letter] += 1;
+      } else {
+        result[letter] = 1;
+      }
+    }
+  }
+
+  return result;
+};
+
+// Test Code
+console.log("LHL", countLetters("LHL"));
+console.log("lighthouse in the house", countLetters("lighthouse in the house"));
+
+console.log("Result 1:");
+const result1 = countLetters("LHL");
+assertEqual(result1.H, 1);
+assertEqual(result1.L, 2);
+
+console.log("Result 2:");
+const result2 = countLetters("lighthouse in the house");
+assertEqual(result2.l, 1);
+assertEqual(result2.i, 2);
+assertEqual(result2.g, 1);
+assertEqual(result2.h, 4);
+assertEqual(result2.t, 2);
+assertEqual(result2.o, 2);
+assertEqual(result2.u, 2);
+assertEqual(result2.s, 2);
+assertEqual(result2.e, 3);
+assertEqual(result2.n, 1);
