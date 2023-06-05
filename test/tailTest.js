50e9714968261cd858930301cfb2a04e5ff6eda1
karvok <kayvee123@gmail.com>
2023-06-05 22:13:21 +0000

    Refactor tailTest.js to use Mocha & Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,18 +1,18 @@
+const assert = require("chai").assert;
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
 
-// Test Code
-// Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
 
-// An array with only one element should yield an empty array for its tail
-const oneWord = ["Testeroo"];
-tail(oneWord);
-assertEqual(oneWord.length, 1);
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse, 'Labs']", () => {
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
 
-// An empty array should yield an empty array for its tail
-const empty = [];
-tail(empty);
-assertEqual(empty.length, 0);
+  it("returns an empty array when passed an array with a single element", () => {
+    assert.deepEqual(tail(['Testeroo']), []);
+  });
+
+  it("returns an empty array when passed an empty array", () => {
+    assert.deepEqual(tail([]), []);
+  });
+
+});
