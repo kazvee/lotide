7362188ea4f8999cfcbd1e0448b233597e3b0273
karvok <kayvee123@gmail.com>
2023-06-07 21:23:34 +0000

    Amend eqArrays.js to have clearer argument names

--- eqArrays.js
+++ eqArrays.js
@@ -1,6 +1,6 @@
-const eqArrays = function(first, second) {
-  for (let i = 0; i < first.length; i++) {
-    if (first[i] !== second[i]) {
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
       return false;
     }
   }
